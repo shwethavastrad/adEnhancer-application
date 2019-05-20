6602be2a4279d3be78936d4f1360b96715b0185b
shwethavastrad <Sweta@1989>
2019-05-19 20:41:18 -0400

    update resources

--- adEnhancer/src/main/java/com/svastrad/adEnhancer/config/SecurityConfig.java
+++ adEnhancer/src/main/java/com/svastrad/adEnhancer/config/SecurityConfig.java
@@ -5,8 +5,6 @@ import com.maxmind.geoip2.DatabaseReader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.util.ResourceUtils;
@@ -20,7 +18,8 @@ import java.io.IOException;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public DatabaseReader databaseReader() throws IOException {
-        File database = new File("/Users/shwethavastrad/Downloads/adEnhancer/src/main/resources/GeoLite2-Country.mmdb");
+
+        File database = ResourceUtils.getFile("classpath:GeoLite2-Country.mmdb");
 
         // This creates the DatabaseReader object. To improve performance, reuse
         // the object across lookups. The object is thread-safe.
